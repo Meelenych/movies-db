27908f9bc87f3e579bd3ea3463a8f442a72222e1
Meelenych <meelenych@gmail.com>
2021-12-09 14:38:08 +0200

    api update

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,8 +1,8 @@
 async function fetchApi(searchQuery) {
 	//URL params
 	const API_KEY = "112103a89f43817e1ae7d8a60b354bbb";
-    const baseUrl = `https://api.themoviedb.org/3/movie/`;
-    const params = `${searchQuery}?api_key=${API_KEY}`;
+    const baseUrl = `https://api.themoviedb.org/3/`;
+    const params = `${searchQuery}/550?api_key=${API_KEY}`;
     
     // https://api.themoviedb.org/3/movie/550?api_key=112103a89f43817e1ae7d8a60b354bbb
 
